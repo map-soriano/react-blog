a15d76dc5c8af4b395e4b7276d14dae87746c997
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-15 18:06:40 +0800

    Implement error handling in Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,17 +2,23 @@ import useFetch from "../hooks/useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const { data: blogs, isPending: isBlogsPending } = useFetch(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
+  const {
+    data: blogs,
+    isPending: isBlogsPending,
+    error: blogsError,
+  } = useFetch("https://jsonplaceholder.typicode.com/posts");
 
-  const { data: authors, isPending: isAuthorsPending } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
+  const {
+    data: authors,
+    isPending: isAuthorsPending,
+    error: authorsError,
+  } = useFetch("https://jsonplaceholder.typicode.com/users");
 
   // FIXME: Type not assignable to authors props
   return (
     <>
+      {blogsError && <h1>An error occured with fetching the blogs</h1>}
+      {authorsError && <h1>An error occured with fetching the authors</h1>}
       {isBlogsPending && <h1>Loading Blogs...</h1>}
       {isAuthorsPending && <p>Loading Authors...</p>}
       {blogs && <BlogList blogs={blogs} authors={authors} />}

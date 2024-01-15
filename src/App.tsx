815fc438d1ccec5fab68968089eac56b50edab10
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-15 18:35:11 +0800

    Implement React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 };

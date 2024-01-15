da7f29226942dc928e466178ccf2b8f6f6726817
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-15 18:02:13 +0800

    Implement error handling in useFetch hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -3,17 +3,28 @@ import { useState, useEffect } from "react";
 const useFetch = (url: string) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(url)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Data not found");
+        }
+        return res.json();
+      })
       .then((data) => {
-        setIsPending(false);
         setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, [url]);
 
-  return { data, isPending };
+  return { data, isPending, error };
 };
 
 export default useFetch;

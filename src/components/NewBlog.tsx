d18da566bdf0bb08384f8809f294e11298e78b6f
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-16 13:17:10 +0800

    Change author input from text to select from users

--- src/components/NewBlog.tsx
+++ src/components/NewBlog.tsx
@@ -1,4 +1,13 @@
+import useFetch from "../hooks/useFetch";
+
 const NewBlog = () => {
+  const {
+    data: authors,
+    isPending,
+    error,
+  } = useFetch("https://jsonplaceholder.typicode.com/users");
+
+  // FIXME: Fix any to receive an object
   return (
     <>
       <form className="mt-3">
@@ -12,7 +21,15 @@ const NewBlog = () => {
           <label htmlFor="author" className="form-label">
             Author
           </label>
-          <input id="author" type="text" className="form-control" />
+          <select id="author" className="form-select">
+            <option key="default" selected disabled>
+              -- Select Author --
+            </option>
+            {authors &&
+              Object.values(authors).map((author: any) => (
+                <option key={author.id}>{author.name}</option>
+              ))}
+          </select>
         </div>
         <div className="mb-3">
           <label htmlFor="blog-body" className="form-label">

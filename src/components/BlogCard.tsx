3ed187ce9386fefdbd516711c773d9f0c18f8ee4
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-17 17:48:16 +0800

    Remove text-decoration from Link

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -7,11 +7,11 @@ type BlogCardProps = {
   id: number;
 };
 
-// TODO: Fix styling of Links
+// TODO: Fix styling of cards (e.g. Add hover effect or reduce number of outputs in a page)
 const BlogCard = ({ title, author, id }: BlogCardProps) => {
   return (
     <>
-      <Link to={`/blog/${id}`} className="card mt-3">
+      <Link to={`/blog/${id}`} className="card mt-3 text-decoration-none">
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p className="card-subtitle">

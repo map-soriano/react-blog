70904d75b01b86e71e7d168fc223b26e37c341a8
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-18 14:45:08 +0800

    Include author's name instead of just userID

--- src/components/BlogView.tsx
+++ src/components/BlogView.tsx
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
-import { URL_POSTS } from "../constants/constants";
-import { Blog } from "../types/types";
+import { URL_POSTS, URL_USERS } from "../constants/constants";
+import { Blog, Author } from "../types/types";
 
 const BlogView = () => {
   const { id } = useParams();
   const { data, isPending, error } = useFetch(`${URL_POSTS}/${id}`);
 
   const blog: Blog | null = data ? (data as Blog) : null;
+
+  const {
+    data: fetchedUser,
+    isPending: userIsPending,
+    error: userError,
+  } = useFetch(blog ? `${URL_USERS}/${blog.userId}` : "");
+
+  const author = fetchedUser ? (fetchedUser as Author).name : null;
 
   return (
     <>
@@ -19,7 +27,9 @@ const BlogView = () => {
           <div className="d-flex align-items-center">
             <div className="me-1 p-2 text-end">
               <h1 className="fs-1">{blog.title}</h1>
-              <p>{blog.userId}</p>
+              {userIsPending && <p>Loading...</p>}
+              {userError && <p>{userError}</p>}
+              {author && <p>{author}</p>}
             </div>
             <div className="ms-1 p-2">
               <p className="fs-5">

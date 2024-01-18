aed40925e008ec0e710074558d854225c97b69b3
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-18 18:08:52 +0800

    Implement pagination on BlogList component

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import BlogCard from "./BlogCard";
 
 import { Blog, Author } from "../types/types";
@@ -8,9 +9,25 @@ type BlogListProps = {
 };
 
 const BlogList = ({ blogs, authors }: BlogListProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentBlogs = blogs.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
   return (
     <>
-      {Object.values(blogs).map((blog) => (
+      {Object.values(currentBlogs).map((blog) => (
         <BlogCard
           key={blog.id}
           id={blog.id}
@@ -22,6 +39,22 @@ const BlogList = ({ blogs, authors }: BlogListProps) => {
           }
         />
       ))}
+      <div className="container d-flex justify-content-evenly mt-5">
+        <button
+          className="btn btn-primary"
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={handleNextPage}
+          disabled={endIndex >= blogs.length}
+        >
+          Next
+        </button>
+      </div>
     </>
   );
 };
